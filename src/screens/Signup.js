b89c9c41feda4dc6269bc0e8f316bb42c4eab149
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2023-04-12 23:37:56 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -21,7 +21,7 @@ export default function Signup() {
     // console.log(latlong)
     let [lat, long] = latlong
     console.log(lat, long)
-    const response = await fetch("https://crunchy-backend-orikvjnuy-1siikaa.vercel.app/getlocation", {
+    const response = await fetch("https://crunchy-backend-mytf1pr23-1siikaa.vercel.app/getlocation", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -37,7 +37,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://crunchy-backend-orikvjnuy-1siikaa.vercel.app/createuser", {
+    const response = await fetch("https://crunchy-backend-mytf1pr23-1siikaa.vercel.app/createuser", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
